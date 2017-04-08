960060e71f49b52440163f262148dcadb01e6a79
ascott02 <ascott02@gmail.com>
2017-04-08 15:14:46 -0700

    a little bit of code cleanup

--- SnakeGame/src/snakegame/Snakey.java
+++ SnakeGame/src/snakegame/Snakey.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package snakegame;
 
 import java.util.ArrayList;
@@ -20,63 +15,53 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import java.util.Iterator;
 import javafx.animation.AnimationTimer;
-// import javafx.event.ActionEvent;
-// import javafx.scene.control.Button;
-// import javafx.scene.layout.StackPane;
-// import javafx.scene.image.ImageView;
-// import javafx.scene.SnapshotParameters;
-
 import javafx.scene.paint.ImagePattern;
 
-
-
-/**
- *
- * @author ascott
- */
 public class Snakey extends Application {
-	int velX = 0;
-	int velY = 0;
 
-	int getVelX() {
-		return velX;
+	// Background Velocity, Accessors, and Settings
+	int bgVelX = 0;
+	int bgVelY = 0;
+
+	int getBGVelX() {
+		return bgVelX;
 	}
 
-	int getVelY() {
-		return velY;
+	int getBGVelY() {
+		return bgVelY;
 	}
 
-	void setVelX(int num) {
-		velX = num;
+	void setBGVelX(int num) {
+		bgVelX = num;
 	}
 
-	void setVelY(int num) {
-		velY = num;
+	void setBGVelY(int num) {
+		bgVelY = num;
 	}
 
-	int X = 0;
-	int Y = 0;
+	// Background Position, Accessors, and Setters
+	int bgX = 0;
+	int bgY = 0;
 
-	int getX() {
-		return X;
+	int getBGX() {
+		return bgX;
 	}
 
-	int getY() {
-		return Y;
+	int getBGY() {
+		return bgY;
 	}
 
-	void setX(int num) {
-		X = num;
+	void setBGX(int num) {
+		bgX = num;
 	}
 
-	void setY(int num) {
-		Y = num;
+	void setBGY(int num) {
+		bgY = num;
 	}
 
-	
 	@Override
 	public void start(Stage primaryStage) {
-		
+
 		// StackPane root = new StackPane();
 		Group root = new Group();
 		int WindowWidth = 640;
@@ -86,161 +71,120 @@ public class Snakey extends Application {
 		Scene theScene = new Scene(root, WindowWidth, WindowHeight);
 		primaryStage.setTitle("Snakey!");
 		primaryStage.setScene(theScene);
-
-		// Canvas canvas = new Canvas(4096, 4096);
 		Canvas canvas = new Canvas(WindowWidth, WindowHeight);
 
-        	ArrayList<String> input = new ArrayList<String>();
-
-		// Image space = new Image("space.png");
+		ArrayList<String> input = new ArrayList<String>();
 		Image cracked = new Image("stars5.jpg");
-		theScene.setFill(new ImagePattern(cracked, 
-			((theScene.getWidth()/2)-32), 
-			((theScene.getHeight()/2)-32), .6, .6, true));
-
+		theScene.setFill(new ImagePattern(cracked,
+			((theScene.getWidth() / 2) - 32),
+			((theScene.getHeight() / 2) - 32), .6, .6, true));
 		root.getChildren().add(canvas);
 
 		theScene.setOnKeyPressed(
-		    new EventHandler<KeyEvent>()
-		    {
-			public void handle(KeyEvent e)
-			{
-			    String code = e.getCode().toString();
-			    if ( !input.contains(code) )
-				input.add( code );
+			new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+				if (!input.contains(code)) {
+					input.add(code);
+				}
 			}
-		    });
+		});
 
 		theScene.setOnKeyReleased(
-		    new EventHandler<KeyEvent>()
-		    {
-			public void handle(KeyEvent e)
-			{
-			    String code = e.getCode().toString();
-			    input.remove( code );
+			new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+				input.remove(code);
 			}
-		    });
+		});
 
-	        GraphicsContext gc = canvas.getGraphicsContext2D();
+		GraphicsContext gc = canvas.getGraphicsContext2D();
 
-		Font theFont = Font.font( "Helvetica", FontWeight.BOLD, 24 );
-		gc.setFont( theFont );
-		gc.setFill( Color.YELLOW);
-		gc.setStroke( Color.BLACK );
+		Font theFont = Font.font("Helvetica", FontWeight.BOLD, 24);
+		gc.setFont(theFont);
+		gc.setFill(Color.YELLOW);
+		gc.setStroke(Color.BLACK);
 		gc.setLineWidth(1);
 
 		Sprite theSnake = new Sprite();
 		theSnake.setImage("snake_head_red.png");
-		theSnake.setPosition((theScene.getWidth()/2)-32, (theScene.getHeight()/2)-32, 180);
-
-		// Sprite theBG = new Sprite();
-		// theBG.setImage("cracked.png");
-		// theBG.setPosition((theScene.getWidth()/2)-32, (theScene.getHeight()/2)-32, 180);
+		theSnake.setPosition((theScene.getWidth() / 2) - 32, (theScene.getHeight() / 2) - 32);
+		theSnake.setAngle(180);
 
 		ArrayList<Sprite> appleList = new ArrayList<Sprite>();
 
-		for (int i = 0; i < 15; i++)
-		{
-		    Sprite apple = new Sprite();
-		    apple.setImage("apple.png");
-		    double px = 350 * Math.random() + 50;
-		    double py = 350 * Math.random() + 50;          
-		    apple.setPosition(px, py, 90);
-		    appleList.add( apple );
+		for (int i = 0; i < 15; i++) {
+			Sprite apple = new Sprite();
+			apple.setImage("apple.png");
+			double px = 350 * Math.random() + 50;
+			double py = 350 * Math.random() + 50;
+			apple.setPosition(px, py);
+			appleList.add(apple);
 		}
 
-		LongValue lastNanoTime = new LongValue( System.nanoTime() );
-
+		LongValue lastNanoTime = new LongValue(System.nanoTime());
 		IntValue score = new IntValue(0);
 
-		new AnimationTimer()
-		{
-		    public void handle(long currentNanoTime)
-		    {
-			// calculate time since last update.
-			double elapsedTime = (currentNanoTime - lastNanoTime.value) / 1000000000.0;
-			lastNanoTime.value = currentNanoTime;
+		new AnimationTimer() {
 
-			// game logic
+			public void handle(long currentNanoTime) {
 
-			// theSnake.setVelocity(0,0);
+				// calculate time since last update.
+				double elapsedTime = (currentNanoTime - lastNanoTime.value) / 1000000000.0;
+				lastNanoTime.value = currentNanoTime;
 
-			if (input.contains("LEFT"))
-			{
-			    // theSnake.setPosition(-250,0, 270);
-			    // theSnake.setVelocity(-250,0, 270, theScene);
-			    theSnake.setVelocity(0,0, 270, theScene, -250, 0);
-			    // theBG.setVelocity(-200, 0, 270, theScene);
-			    setVelX(1);
-			    setX(getX()+10);
+				// game logic
+				if (input.contains("LEFT")) {
+					theSnake.setAngle(270);
+					setBGVelX(10);
+					setBGX(getBGX() + 10);
+				}
+
+				if (input.contains("RIGHT")) {
+					theSnake.setAngle(90);
+					setBGVelX(-10);
+					setBGX(getBGX() - 10);
+				}
+
+				if (input.contains("UP")) {
+					theSnake.setAngle(0);
+					setBGVelY(10);
+					setBGY(getBGY() + 10);
+				}
+
+				if (input.contains("DOWN")) {
+					theSnake.setAngle(180);
+					setBGVelY(-10);
+					setBGY(getBGY() - 10);
+				}
+
+				// collision detection
+				Iterator<Sprite> appleIter = appleList.iterator();
+				while (appleIter.hasNext()) {
+					Sprite apple = appleIter.next();
+					if (theSnake.intersects(apple)) {
+						appleIter.remove();
+						score.value++;
+					}
+				}
+
+				// render
+				gc.clearRect(0, 0, WindowWidth, WindowHeight);
+				theScene.setFill(new ImagePattern(cracked,
+					(getBGX() + getBGVelX()), (getBGY() + getBGVelY()),
+					cracked.getWidth(), cracked.getHeight(), false));
+				theSnake.render(gc);
+
+				for (Sprite apple : appleList) {
+					apple.render(gc);
+				}
+
+				String pointsText = "Score: " + (100 * score.value);
+				gc.fillText(pointsText, 360, 24);
+				gc.strokeText(pointsText, 360, 24);
 			}
-
-			if (input.contains("RIGHT"))
-			{
-			    // theSnake.setPosition(250,0, 90);
-			    // theSnake.setVelocity(250,0, 90, theScene);
-			    theSnake.setVelocity(0, 0, 90, theScene, 250, 0);
-			    setVelX(-1);
-			    setX(getX()-10);
-			}
-
-			if (input.contains("UP"))
-			{
-			    // theSnake.setPosition(0,-250, 0);
-			    // theSnake.setVelocity(0,-250, 0, theScene);
-			    theSnake.setVelocity(0, 0, 0, theScene, 0, -250);
-			    setVelY(1);
-			    setY(getY()+10);
-			}
-
-			if (input.contains("DOWN"))
-			{
-			    // theSnake.setPosition(0,250, 180);
-			    // theSnake.setVelocity(0,250, 180, theScene);
-			    theSnake.setVelocity(0, 0, 180, theScene, 0, 250);
-			    setVelY(-1);
-			    setY(getY()-10);
-			}
-
-			// collision detection
-
-			Iterator<Sprite> appleIter = appleList.iterator();
-			while ( appleIter.hasNext() )
-			{
-			    Sprite apple = appleIter.next();
-			    if ( theSnake.intersects(apple) )
-			    {
-				appleIter.remove();
-				score.value++;
-			    }
-			}
-
-			// render
-
-			gc.clearRect(0, 0, WindowWidth, WindowHeight);
-			// gc.drawImage(space, 0, 0);
-			// gc.drawImage(cracked, 0, 0);
-			// gc.setFill(new ImagePattern(cracked, 0.2, 0.2, .6, .6, true));
-
-
-			theScene.setFill(new ImagePattern(cracked, getX() + getVelX(), getY() + getVelY(), cracked.getWidth(), cracked.getHeight(), false));
-
-			theSnake.render( gc );
-
-			for (Sprite apple : appleList )
-			    apple.render( gc );
-
-			String pointsText = "Score: " + (100 * score.value);
-			gc.fillText( pointsText, 360, 24);
-			gc.strokeText( pointsText, 360, 24);
-		    }
 		}.start();
-
-
-		primaryStage.show();        
-
-	
-
+		primaryStage.show();
 	}
 
 	/**
@@ -249,5 +193,4 @@ public class Snakey extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 }
--- SnakeGame/src/snakegame/Sprite.java
+++ SnakeGame/src/snakegame/Sprite.java
@@ -9,105 +9,96 @@ import javafx.scene.SnapshotParameters;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 
-public class Sprite
-{
-    private Image image;
-    private ImageView theImageView;
-    private double positionX;
-    private double positionY;    
-    private double bgPositionX;
-    private double bgPositionY;    
-    private double velocityX;
-    private double velocityY;
-    private double bgVelocityX;
-    private double bgVelocityY;
-    private double width;
-    private double height;
-    private double angle;
-    private final ImageView head;
+public class Sprite {
 
-    public Sprite()
-    {
-        positionX = 0;
-        positionY = 0;    
-        velocityX = 0;
-        velocityY = 0;
-	head = new ImageView(new Image("snake_head_red.png"));
-    }
+	private Image image;
+	private ImageView theImageView;
+	private double positionX;
+	private double positionY;
+	private double bgPositionX;
+	private double bgPositionY;
+	private double velocityX;
+	private double velocityY;
+	private double bgVelocityX;
+	private double bgVelocityY;
+	private double width;
+	private double height;
+	private double angle;
+	private final ImageView head;
 
-    public void setImage(Image i)
-    {
-        image = i;
-        width = i.getWidth();
-        height = i.getHeight();
-    }
+	public Sprite() {
+		positionX = 0;
+		positionY = 0;
+		velocityX = 0;
+		velocityY = 0;
+		angle = 0;
+		head = new ImageView(new Image("snake_head_red.png"));
+	}
 
-    public Image getImage()
-    {
-        return image;
-    }
+	public void setImage(Image i) {
+		image = i;
+		width = i.getWidth();
+		height = i.getHeight();
+	}
 
-    public double getAngle()
-    {
-        return angle;
-    }
+	public Image getImage() {
+		return image;
+	}
 
-    public void setImage(String filename)
-    {
-        Image i = new Image(filename);
-        setImage(i);
-    }
+	public double getAngle() {
+		return angle;
+	}
 
-    public void setPosition(double x, double y, double angle)
-    {
-        positionX = x;
-        positionY = y;
-	angle = angle;
-    }
+	public void setAngle(double newAngle) {
+		angle = newAngle;
 
-    public void setVelocity(double x, double y, double angle, Scene scene, double bgx, double bgy )
-    {
-        velocityX = x;
-        velocityY = y;
+		// Rotate the head to the new angle
+		head.setRotate(newAngle);
+		SnapshotParameters params = new SnapshotParameters();
+		params.setFill(Color.TRANSPARENT);
+		Image rotatedImage = head.snapshot(params, null);
+		setImage(rotatedImage);
+	}
 
-	// Rotate the head to the new angle
-	head.setRotate(angle);
-	SnapshotParameters params = new SnapshotParameters();
-	params.setFill(Color.TRANSPARENT);
-	Image rotatedImage = head.snapshot(params, null);
-	setImage(rotatedImage);
-    }
+	public void setImage(String filename) {
+		Image i = new Image(filename);
+		setImage(i);
+	}
 
-    public void addVelocity(double x, double y)
-    {
-        velocityX += x;
-        velocityY += y;
-    }
+	public void setPosition(double x, double y) {
+		positionX = x;
+		positionY = y;
+	}
 
-    public void update(double time, Scene scene)
-    {
-        positionX += velocityX * time;
-        positionY += velocityY * time;
-    }
+	public void setVelocity(double x, double y) {
+		velocityX = x;
+		velocityY = y;
+	}
 
-    public void render(GraphicsContext gc)
-    {
-        gc.drawImage( image, positionX, positionY );
-    }
+	public void addVelocity(double x, double y) {
+		velocityX += x;
+		velocityY += y;
+	}
 
-    public Rectangle2D getBoundary()
-    {
-        return new Rectangle2D(positionX,positionY,width,height);
-    }
+	public void update(double time, Scene scene) {
+		positionX += velocityX * time;
+		positionY += velocityY * time;
+	}
 
-    public boolean intersects(Sprite s)
-    {
-        return s.getBoundary().intersects( this.getBoundary() );
-    }
-    
-    public String toString()
-    {
-        return " Position: [" + positionX + "," + positionY + "]" 
-        + " Velocity: [" + velocityX + "," + velocityY + "]";
-    }
+	public void render(GraphicsContext gc) {
+		gc.drawImage(image, positionX, positionY);
+	}
+
+	public Rectangle2D getBoundary() {
+		return new Rectangle2D(positionX, positionY, width, height);
+	}
+
+	public boolean intersects(Sprite s) {
+		return s.getBoundary().intersects(this.getBoundary());
+	}
+
+	public String toString() {
+		return " Position: [" + positionX + "," + positionY + "]"
+			+ " Velocity: [" + velocityX + "," + velocityY + "]";
+	}
 }
