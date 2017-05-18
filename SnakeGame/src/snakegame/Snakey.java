4a120d5eb933bb748c207615bfae21e4b9d65f78
zi <>
2017-05-18 13:01:18 -0700

    remove empty classes, and move background class to background.java from snakey

--- SnakeGame/src/snakegame/Snakey.java
+++ SnakeGame/src/snakegame/Snakey.java
@@ -451,6 +451,8 @@ public class Snakey extends Application {
         launch(args);
     }
 }
+
+
 /**
  * Turns a long into a class with a public data field
  * @author kamuela94
@@ -477,80 +479,4 @@ class IntValue {
     }
 }
 
-/**
- * Holds the information for the background velocities and positions
- * @author kamuela94
- */
-class Background {
 
-    // Background Position, Accessors, and Setters
-    public double bgX = 0.0;
-    public double bgY = 0.0;
-    // Background Velocites, Accessors and Setters
-    double bgVelX = 0.0;
-    double bgVelY = 0.0;
-    
-    /**
-    * 
-    * @return The x value of the background velocity
-    */
-    double getBGVelX() {
-        return bgVelX;
-    }
-
-    /**
-     * 
-     * @return The y value of the background velocity
-     */
-    double getBGVelY() {
-        return bgVelY;
-    }
-
-    /**
-     * Sets the x value of the background velocity
-     * @param num the double to be set to background velocity X
-     */
-    void setBGVelX(double num) {
-        bgVelX = num;
-    }
-
-    /**
-     * Sets the y value of the background velocity
-     * @param num the double to be set to background velocity Y
-     */
-    void setBGVelY(double num) {
-        bgVelY = num;
-    }
-
-    /**
-     * 
-     * @return the position of the background on the x plane
-     */
-    double getBGX() {
-        return bgX;
-    }
-
-    /**
-     * 
-     * @return the position of the background on the y plane
-     */
-    double getBGY() {
-        return bgY;
-    }
-    
-    /**
-     * sets the position of the background to value num on the x plane
-     * @param num 
-     */
-    void setBGX(double num) {
-        bgX = num;
-    }
-
-    /**
-     * sets the position of the background to value num on the y plane
-     * @param num 
-     */
-    void setBGY(double num) {
-        bgY = num;
-    }
-}
